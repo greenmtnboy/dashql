e71bb4310e15ebdacdecc8c56878a1c07f16cac6
greenmtnboy <ethan.dickinson@gmail.com>
2016-03-24 20:02:59 -0400

    initializing

---
+++ Core.py
@@ -0,0 +1,62 @@
+import sqlite3
+import re
+
+##find the source of table
+def fromClause(script):
+    fc = re.search('from (.*?)[\s\n]',script)
+    if fc:
+        return fc.group(1)
+
+##find joins
+def joinClause(script):
+    fc = re.search('join (.*?)[\s\n]',script)
+    if fc:
+        return fc.group(1)
+
+##find query columns
+def selectClause(script):
+    fc = re.search('select(.*?)into|from', script,re.DOTALL)
+    if fc:
+        return fc.group(1)
+
+# let's define a script we need to parse
+examplescript="""
+select orid, orcompletedate, opprsku
+INTO #tmpTable
+from csn_order..vwOrder a
+inner join csn_order..vworderproduct b on a.orid=b.oporid
+where orcompletedate>'1/1/2015';
+
+
+select
+opprsku, count(*) count
+into #finaltable
+from #tmptable
+group by opprsku
+having count(*)>100;
+"""
+
+parsed=examplescript.split(';')
+print(parsed)
+for i in parsed:
+   # print (i)
+    print(fromClause(i))
+    print(joinClause(i))
+    print(selectClause(i))
+
+
+#this will hold all our parsedprocedure information
+#db = sqlite3.connect('data/coredb')
+
+# let's keep it in memory for our purposes
+db = sqlite3.connect(':memory:')
+
+
+cursor = db.cursor()
+'''
+cursor.execute(
+CREATE TABLE
+)
+'''
+
+db.close()
